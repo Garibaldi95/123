d632faa1dc0c3fd6a2f7c4acdba611581051053c
Garibaldi95 <62938622+Garibaldi95@users.noreply.github.com>
2020-08-07 22:19:19 +0300

    Add files via upload

---
+++ test3.php
@@ -0,0 +1,24 @@
+<?php
+
+//Устанавливаем доступы к базе данных:
+$host = 'localhost'; //имя хоста, на локальном компьютере это localhost
+$user = 'root'; //имя пользователя, по умолчанию это root
+$password = 'root'; //пароль, по умолчанию пустой
+$db_name = 'tasks'; //имя базы данных
+
+//Соединяемся с базой данных используя наши доступы:
+$link = mysqli_connect($host, $user, $password, $db_name);
+
+$query = "SELECT* FROM taskbook";
+
+//Делаем запрос к БД, результат запроса пишем в $result:
+$result = mysqli_query($link, $query) or die(mysqli_error($link));
+
+//Преобразуем то, что отдала нам база в нормальный массив PHP $data:
+for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);
+
+echo '<pre>';
+print_r($data);
+echo '</pre>';
+?>
+
